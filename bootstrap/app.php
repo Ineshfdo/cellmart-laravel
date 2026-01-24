4b7b0ddff9fed6d1d4c1b02c8b1104738f788681
Ineshfdo <ineshfernando643@gmail.com>
2026-01-24 13:29:58 +0530

    Fix the image Upload issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust proxies for HTTPS detection
+        $middleware->trustProxies(at: '*');
+        
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         ]);
