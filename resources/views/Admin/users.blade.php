94632594a81f8f5a8ec733d51ae60dd49da13f0e
Ineshfdo <ineshfernando643@gmail.com>
2026-01-24 13:32:02 +0530

    Fix

--- resources/views/Admin/users.blade.php
+++ resources/views/Admin/users.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             
-            <!-- Success/Error Messages -->
+            <!-- Success/Error Message -->
             @if(session('success'))
                 <div class="mb-4 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded-lg relative" role="alert">
                     <span class="block sm:inline">{{ session('success') }}</span>
@@ -95,3 +95,4 @@
         </div>
     </div>
 </x-app-layout>
+
